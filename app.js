1155e78b046a4c81a51b2b2397a8f4bbbed6eb6d
makyos <makyos@gmail.com>
2017-08-14 23:03:43 +0900

    3rd

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var container = document.createElement("div");
 container.setAttribute('class', 'container');
 elem.appendChild(container)
 
-for (var i = 0; i < 3; i++) {
+for (var i = 0; i < 2; i++) {
 
     var div = document.createElement("div");
     container.appendChild(div);
@@ -15,8 +15,8 @@ for (var i = 0; i < 3; i++) {
 
     // var width = window.innerWidth;
     // var height = window.innerHeight;
-    var width = 400;
-    var height = 200;
+    var width = 250;
+    var height = 150;
 
     var scene = new THREE.Scene();
 
@@ -36,18 +36,35 @@ for (var i = 0; i < 3; i++) {
     var sphere = new THREE.Mesh(geometry, material);
     scene.add(sphere);
 
-    // var camera = new THREE.PerspectiveCamera(75, width / height, 1, 1000);
-    var camera = new THREE.PerspectiveCamera();
+    var camera = new THREE.PerspectiveCamera(75, width / height, 1, 1000);
     camera.position.set(0, 0, 0.1);
     camera.lookAt(sphere.position);
 
     var renderer = new THREE.WebGLRenderer();
-    // renderer.setSize(width, height);
+    renderer.setSize(width, height);
     renderer.setClearColor({ color: 0x000000 });
     div.appendChild(renderer.domElement);
     renderer.render(scene, camera);
 
-
+    var controls = new THREE.OrbitControls(camera,renderer.domElement);
+    
+      function render(){
+        requestAnimationFrame(render);
+        sphere.rotation.y += 0.05 * Math.PI/180;
+        //画面リサイズ対応
+        window.addEventListener( 'resize', onWindowResize, false );
+        renderer.render(scene,camera);
+        controls.update();
+      }
+      render();
+    
+    
+      function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+      }
+    
 }
 
 function fileListDirectory(files) {
